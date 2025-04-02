ccb7d0605781b3e00b54350ed6fac98f3d722582
Hsin-Wen Chang <bow1226@gmail.com>
2025-04-02 19:48:41 +0800

    feat: Implement declarative macros for the game engine

---
+++ starter/my_game_engine/src/main.rs
@@ -0,0 +1,40 @@
+macro_rules! spawn_sprite {
+    ($x:expr, $y:expr, $width:expr, $height:expr) => {{
+        let sprite = create_sprite($x, $y, $width, $height);
+        render_sprite(sprite);
+    }};
+}
+
+macro_rules! on_key_press {
+    ($key:expr, $action:block) => {{
+        if get_key($key) == GLFW_PRESS {
+            $action
+        }
+    }};
+}
+
+macro_rules! tick {
+    ($duration:expr) => {{
+        update_game_window();
+        std::thread::sleep(std::time::Duration::from_millis($duration));
+    }};
+}
+
+macro_rules! start_window_and_game_loop {
+    ($width:expr, $height:expr) => {{
+        create_window($width, $height);
+        while !window_should_close() {
+            tick!(16); 
+        }
+    }};
+}
+
+fn main() {
+    start_window_and_game_loop!(800, 600);
+
+    spawn_sprite!(100, 150, 50, 50);
+
+    on_key_press!(GLFW_KEY_SPACE, {
+        println!("Space key was pressed!");
+    });
+}
