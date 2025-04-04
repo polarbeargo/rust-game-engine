611ab291edf09f164135e0c0b4f5bd8fdbba3929
Hsin-Wen Chang <bow1226@gmail.com>
2025-04-04 12:43:27 +0800

    test: Refactor tests to use macro

--- starter/my_game_engine/src/lib.rs
+++ starter/my_game_engine/src/lib.rs
@@ -4,18 +4,11 @@ mod tests {
 
     #[test]
     fn test_simple_game_loop() {
-        
-        create_window(800, 600);
-
-        while !window_should_close() {
-            update_game_window();
-            std::thread::sleep(std::time::Duration::from_millis(16));
-        }
+        start_window_and_game_loop!(800, 600);
     }
 
     #[test]
     fn test_sprite_rendering() {
-     
         create_window(800, 600);
         let sprite = create_sprite(100, 100, 50, 50);
         render_sprite(sprite);
@@ -27,33 +20,30 @@ mod tests {
     #[test]
     fn test_screen_clearing() {
         create_window(800, 600);
- 
+
         let red_sprite = create_sprite(100, 100, 50, 50);
         render_sprite(red_sprite);
 
-        std::thread::sleep(std::time::Duration::from_secs(5)); 
+        std::thread::sleep(std::time::Duration::from_secs(5));
         clear_screen();
 
         let green_sprite = create_sprite(100, 100, 50, 50);
         render_sprite(green_sprite);
 
-        std::thread::sleep(std::time::Duration::from_secs(2)); 
-
+        std::thread::sleep(std::time::Duration::from_secs(2));
         close_window();
     }
 
     #[test]
     fn test_key_presses() {
- 
         create_window(800, 600);
-        
+
         let mut key_pressed = false;
 
-  
         while !window_should_close() {
-            if get_key(GLFW_KEY_SPACE) == GLFW_PRESS {
+            on_key_press!(GLFW_KEY_SPACE, {
                 key_pressed = true;
-            }
+            });
 
             if key_pressed {
                 break;
@@ -69,17 +59,15 @@ mod tests {
     #[test]
     fn test_sprite_position_update() {
         create_window(800, 600);
-        
-        let sprite = create_sprite(100, 100, 50, 50); 
+
+        let sprite = create_sprite(100, 100, 50, 50);
         let mut position = 0;
 
         while !window_should_close() {
-           
-            update_sprite_position(sprite, position, 100); 
+            update_sprite_position(sprite, position, 100);
             render_sprite(sprite);
 
             std::thread::sleep(std::time::Duration::from_millis(16));
-
             position += 1;
             clear_screen();
         }
