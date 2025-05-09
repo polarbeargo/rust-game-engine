0df76386eb42a4f18d5c6cbe774d2fa446bdcf96
Hsin-Wen Chang <bow1226@gmail.com>
2025-05-09 18:31:55 +0800

    test: Implement tests for Macros

--- starter/my_game_engine/src/lib.rs
+++ starter/my_game_engine/src/lib.rs
@@ -1,8 +1,22 @@
-pub mod ffi;
+mod ffi;
+#[macro_use]
+mod macros;
+
+pub struct Sprite {
+    pub x: f32,
+    pub y: f32,
+    pub width: i32,
+    pub height: i32,
+    pub r: i32,
+    pub g: i32,
+    pub b: i32,
+}
 
 #[cfg(test)]
 mod tests {
     use crate::ffi::*;
+    use crate::macros::*;
+    use crate::Sprite;
 
     fn setup_window() -> *mut std::ffi::c_void {
         let window = rust_create_window("Test Window", 800, 600);
@@ -15,17 +29,6 @@ mod tests {
             rust_update_game_window();
         }
         rust_terminate_glfw();
-    }
-
-    #[test]
-    fn test_get_key() {
-        let window = setup_window();
-        rust_simulate_key_press(GLFW_KEY_SPACE, GLFW_PRESS);
-
-        let key_state = rust_get_key(window, GLFW_KEY_SPACE);
-        assert_eq!(key_state, true, "Expected GLFW_KEY_SPACE to be pressed");
-
-        run_game_loop();
     }
 
     #[test]
@@ -53,13 +56,6 @@ mod tests {
     }
 
     #[test]
-    fn test_clear_screen() {
-        let _window = setup_window();
-        rust_clear_screen();
-        run_game_loop();
-    }
-
-    #[test]
     fn test_update_sprite_position() {
         let _window = setup_window();
         let sprite = rust_create_sprite(100.0, 100.0, 50, 50, 255, 0, 0);
@@ -80,5 +76,41 @@ mod tests {
         }
 
         rust_terminate_glfw();
+    }
+
+    #[test]
+    fn test_spawn_sprite() {
+        let _window = setup_window();
+        spawn_sprite!(100.0, 150.0, 50, 50, 255, 0, 0);
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        rust_clear_screen();
+    }
+
+    #[test]
+    fn test_tick() {
+        let _window = setup_window();
+        tick!(100);
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        rust_clear_screen();
+    }
+
+    #[test]
+    fn test_change_sprite_color() {
+        let window = setup_window();
+
+        let mut sprite = Sprite {
+            x: 100.0,
+            y: 100.0,
+            width: 50,
+            height: 50,
+            r: 255,
+            g: 0,
+            b: 0,
+        };
+
+        change_sprite_color!(sprite, 0, 0, 255);
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        run_game_loop();
+        rust_clear_screen();
     }
 }
